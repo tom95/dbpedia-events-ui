2b2f4135bf4697c41e15be1de1aff3740b3d3db3
Tom Beckmann <tomjonabc@gmail.com>
2016-05-07 00:47:50 +0200

    Initial timeline design try

--- public/js/controllers/TimelineController.js
+++ public/js/controllers/TimelineController.js
@@ -1,4 +1,6 @@
 
 angular.module('dbpedia-events-ui').controller('TimelineController', ['$scope', function($scope) {
+	$scope.availableDays = [new Date(), new Date(+new Date() - 60*60*1000*24*5)];
+	$scope.activeDay = new Date();
 }]);
 
