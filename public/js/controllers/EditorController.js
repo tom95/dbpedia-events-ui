d31441e1a5dcafdb52ac9ea5ef3b54e98870869a
Nils Strelow <djnilse@gmail.com>
2016-05-25 13:31:25 +0200

    remove unneccessary things

--- public/js/controllers/EditorController.js
+++ public/js/controllers/EditorController.js
@@ -1,10 +1,11 @@
 var app = angular.module('dbpedia-events-ui');
 
 app.controller('EditorController', ['$scope', function($scope) {
-  $scope.submit = function($scope) {
-    alert("Digest Template successfully saved!");
-  };
-  $scope.change = function($scope) {
+    $scope.submit = function() {
+        console.log($scope.template);
+        console.log(angular.element(template).html());
+    };
+    $scope.change = function() {
 
-  };
+    };
 }]);
