44c1b76b90874f100d8aa5ed980f6a73a86db32e
Nils Strelow <djnilse@gmail.com>
2016-05-25 21:04:51 +0200

    ngEnter directive

---
+++ public/js/directives/ngEnter.js
@@ -0,0 +1,12 @@
+angular.module('dbpedia-events-ui').directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if (event.which == 13) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.keyEnter, {'event': event});
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
