96ec350a644c72d23d69bef8b710a5ff6bf5a2fc
Nils Strelow <djnilse@gmail.com>
2016-06-02 22:57:57 +0200

    added load function

--- public/js/controllers/EditorController.js
+++ public/js/controllers/EditorController.js
@@ -1,16 +1,16 @@
 var app = angular.module('dbpedia-events-ui');
 
-app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
+app.controller('EditorController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.ontologies = [];
     $scope.filters = [];
 
-    $scope.addOntology = function() {
+    $scope.addOntology = function () {
         $scope.ontologies.push($scope.newOntology);
         $scope.newOntology = "";
     };
 
-    $scope.removeOntology = function(ontology) {
+    $scope.removeOntology = function (ontology) {
         console.log($scope.ontologies);
         var index = $scope.ontologies.indexOf(ontology);
         if (index > -1) {
@@ -19,12 +19,12 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
         console.log($scope.ontologies);
     };
 
-    $scope.addFilter = function() {
+    $scope.addFilter = function () {
         $scope.filters.push($scope.newFilter);
         $scope.newFilter = "";
     };
 
-    $scope.removeFilter = function(filter) {
+    $scope.removeFilter = function (filter) {
         console.log($scope.filters);
         var index = $scope.filters.indexOf(filter);
         if (index > -1) {
@@ -33,7 +33,31 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
         console.log($scope.filters);
     };
 
-    $scope.submit = function() {
+    $scope.templates = [];
+
+    $scope.loadTemplates = function () {
+        $http({
+            method: 'GET',
+            url: '/template'
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            $scope.templates = response.data;
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+    };
+    
+    $scope.loadTemplates();
+
+    $scope.loadTemplateIntoEditor = function (template) {
+        $scope.name = template.name;
+        $scope.description = template.description;
+        $scope.ontologies = template.ontologies;
+        $scope.descriptionTemplate = template.descriptionTemplate;
+        $scope.rankWeight = template.rankWeight;
+    };
+
+    $scope.saveTemplate = function () {
 
         var templateText = angular.element(document.getElementById("template")).text();
         var queryString = angular.element(document.getElementById("queryString")).text();
@@ -51,10 +75,6 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
             "rankWeight": $scope.rankWeight
         };
 
-        console.log(template)
-
-
-
         $http({
             method: 'POST',
             url: '/template',
@@ -65,10 +85,14 @@ app.controller('EditorController', ['$scope', '$http', function($scope, $http) {
             headers: {
                 'Content-Type': 'application/json'
             },
-            success: function(data, status) {
+            success: function (data, status) {
                 console.log("Posted template successfully");
             }
         });
+    };
+
+    $scope.test = function () {
+
     };
 
 
